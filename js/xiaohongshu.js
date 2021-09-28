925398dffd4605e11ad7a79dbbb33e7104d5b7e3
XIAOHUIJI <2712403383@qq.com>
2021-09-28 20:14:56 +0800

    first commit

---
+++ js/xiaohongshu.js
@@ -0,0 +1,326 @@
+function over1(){
+    var btn = document.getElementById("android");
+    btn.style.display="block";
+    
+    
+    }
+    function out1(){
+    var btn=document.getElementById("android");
+    btn.style.display="none";
+    
+    }
+    function over2(){
+    var btn = document.getElementById("ios");
+    btn.style.display="block";
+    
+    
+    }
+    function out2(){
+    var btn=document.getElementById("ios");
+    btn.style.display="none";
+    
+    }
+    function over3(){
+    var btn = document.getElementById("author");
+    btn.style.display="block";
+    
+    
+    }
+    function out3(){
+    var btn=document.getElementById("author");
+    btn.style.display="none";
+    
+    }
+    function over4(){
+    var btn = document.getElementById("cooperation");
+    btn.style.display="block";
+    
+    
+    }
+    function out4(){
+    var btn=document.getElementById("cooperation");
+    btn.style.display="none";
+    
+    }
+    function over5(){
+    var btn = document.getElementById("language");
+    btn.style.display="block";
+    
+    
+    }
+    function out5(){
+    var btn=document.getElementById("language");
+    btn.style.display="none";
+    
+    }
+
+
+
+    // community**********************************************************************************
+    function lighten1(){
+        var myImg =document.getElementById("heart1")
+        var zan =document.getElementById("zan1")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten2(){
+        var myImg =document.getElementById("heart2")
+        var zan =document.getElementById("zan2")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten3(){
+        var myImg =document.getElementById("heart3")
+        var zan =document.getElementById("zan3")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten4(){
+        var myImg =document.getElementById("heart4")
+        var zan =document.getElementById("zan4")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten5(){
+        var myImg =document.getElementById("heart5")
+        var zan =document.getElementById("zan5")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten6(){
+        var myImg =document.getElementById("heart6")
+        var zan =document.getElementById("zan6")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten7(){
+        var myImg =document.getElementById("heart7")
+        var zan =document.getElementById("zan7")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten8(){
+        var myImg =document.getElementById("heart8")
+        var zan =document.getElementById("zan8")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten9(){
+        var myImg =document.getElementById("heart9")
+        var zan =document.getElementById("zan9")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten10(){
+        var myImg =document.getElementById("heart10")
+        var zan =document.getElementById("zan10")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten11(){
+        var myImg =document.getElementById("heart11")
+        var zan =document.getElementById("zan11")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten12(){
+        var myImg =document.getElementById("heart12")
+        var zan =document.getElementById("zan12")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten13(){
+        var myImg =document.getElementById("heart13")
+        var zan =document.getElementById("zan13")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten14(){  
+        var myImg =document.getElementById("heart14")
+        var zan =document.getElementById("zan14")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten15(){
+        var myImg =document.getElementById("heart15")
+        var zan =document.getElementById("zan15")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten16(){
+        var myImg =document.getElementById("heart16")
+        var zan =document.getElementById("zan16")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function lighten17(){
+        var myImg =document.getElementById("heart17")
+        var zan =document.getElementById("zan17")
+        if(myImg.getAttribute("src")== "./images/heart.png"){
+            myImg.setAttribute("src","./images/heartRed.png");
+            zan.innerText++
+        }else{
+            myImg.setAttribute("src","./images/heart.png");
+            zan.innerText--
+        }
+    
+    }
+    function overandroid(){
+        var btn = document.getElementById("android");
+        btn.style.display="block";
+        
+        
+        }
+    function outandroid(){
+
+        var btn=document.getElementById("android");
+        btn.style.display="none";
+        
+        }
+    function cover(){
+        var cover = document.getElementById("cover");
+        var coverImg = document.getElementById("coverImg");
+        cover.style.display="block";
+        coverImg.style.display="block";
+    }
+    function coverImg(){
+        var cover = document.getElementById("cover");
+        var coverImg = document.getElementById("coverImg");
+        cover.style.display="none";
+        coverImg.style.display="none";
+    }
+    // partner**********************************************************************************************
+    function xs(){
+        var xs = document.getElementById("down");
+        down.style.display="block";
+    }
+    function yc(){
+        var yc = document.getElementById("down");
+        yc.style.display="none";
+    }
+    // social responsibility *************************************
+    function xia(){
+        document.getElementById("diyiyexingwen").style.display="none";
+        document.getElementById("dieryexinwen").style.display="block";
+    }
+    function shang(){
+        document.getElementById("diyiyexingwen").style.display="block";
+        document.getElementById("dieryexinwen").style.display="none";
+    }
+    // About Us************************************************
+    function Aaa() {
+        document.getElementById("shanghai").style.display = "none";
+        document.getElementById("wuhan").style.display = "none";
+        document.getElementById("beijing").style.display = "block";
+    }
+    function Aa() {
+        document.getElementById("beijing").style.display = "none";
+        document.getElementById("wuhan").style.display = "none";
+        document.getElementById("shanghai").style.display = "block";
+    }
+    function Aaaa() {
+        document.getElementById("shanghai").style.display = "none";
+        document.getElementById("beijing").style.display = "none";
+        document.getElementById("wuhan").style.display = "block";
+    }
+    function Aaaaa(){
+        
+            document.getElementById("tx").style.display="block";
+        
+    }
